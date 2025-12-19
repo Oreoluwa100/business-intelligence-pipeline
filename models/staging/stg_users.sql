15f5cd3b8ebeac0bd241f56a2be3c7de94153ac7
Oreoluwa Akinsoyinu <akinsoyinu_oreoluwa@yahoo.com>
2025-12-19 21:30:43 +0000

    other staging models builts

---
+++ models/staging/stg_users.sql
@@ -0,0 +1,34 @@
+{{
+    config(
+        schema='staging',
+        materialized='view',
+        tags=['staging', 'saas_platform']
+    )
+}}
+
+{{
+    config(
+        materialized='view',
+        tags=['staging', 'saas']
+    )
+}}
+
+select
+    user_id,
+    email,
+    company_name,
+    signup_date::date as signup_date,
+    initial_plan,
+    country,
+    industry,
+    employee_count,
+    case employee_count
+        when '1-10' then 'Small Business'
+        when '11-50' then 'Medium Business'
+        when '51-200' then 'Large Business'
+        when '201-500' then 'Enterprise'
+        when '500+' then 'Large Enterprise'
+        else 'Unknown'
+    end as company_size_category,
+    current_date - signup_date::date as days_since_signup
+from {{ source('saas_platform', 'users') }}
